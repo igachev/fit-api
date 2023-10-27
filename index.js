a9d17a510309ccc5c30b8f444f997e7e36047bc7
igachev <ivophoenix1135@gmail.com>
2023-10-27 20:40:18 +0300

    added /users route

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const setupDatabase = require('./config/initDatabase.js')
+const routes = require('./routes.js')
 
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+app.use(routes)
 
 setupDatabase()
 .then(() => {
