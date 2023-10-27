6105f9d0164c6c0998bd826840973006d443e909
igachev <ivophoenix1135@gmail.com>
2023-10-27 20:39:02 +0300

    added endpoints for login/register

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const userService = require('../services/userService.js')
+const {getErrorMessage} = require('../utils/errorMsg.js')
+
+router.post('/register', async(req,res) => {
+    const {email,password} = req.body;
+
+    try {
+        const result = await userService.register(email,password)
+        res.status(201).json(result)
+    } catch (err) {
+        res.status(400).json({message: getErrorMessage(err)})
+    }
+
+})
+
+router.post('/login', async(req,res) => {
+    const {email,password} = req.body;
+
+    try {
+        const result = await userService.login(email,password)
+        res.status(200).json(result)
+    } catch (err) {
+        res.status(400).json({message: getErrorMessage(err)})
+    }
+})
+
+module.exports = router;
