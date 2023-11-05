3aa62c3eafbbbf5d054b8a3e5138577648a2b972
igachev <ivophoenix1135@gmail.com>
2023-11-05 17:31:41 +0200

    edited Exercise model

--- models/Exercise.js
+++ models/Exercise.js
@@ -11,26 +11,7 @@ const exerciseSchema = new mongoose.Schema({
     required: [true,'muscle group is required']
  },
 
- sets: [
-   {
-     setNumber: {
-       type: Number,
-       required: [true, 'set number is required'],
-     },
-     reps: {
-       type: Number,
-       required: [true, 'reps are required'],
-     },
-     weight: {
-       type: Number,
-       required: [true, 'weight is required'],
-     },
-     restTime: {
-       type: String,
-       required: [true, 'rest time is required'],
-     },
-   },
- ]
+
 });
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
