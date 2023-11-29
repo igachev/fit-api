76a70f69e771016356f7c6aa84fa40a29580160c
igachev <ivophoenix1135@gmail.com>
2023-11-29 13:21:03 +0200

    fixed div

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -16,13 +16,13 @@ const sendEmail = async (email, subject, payload) => {
     });
 
 
-    const emailHtml = 
+    const emailHtml = `
     <div>
-        <p>Hi, {payload.name}.</p>
+        <p>Hi, ${payload.name}.</p>
         <p>You requested to reset your password.</p>
         <p> Please, click the link below in next 15 minutes to reset your password.</p>
-        <a href={payload.link}>Reset password</a>,
-    </div>;
+        <a href="${payload.link}">Reset password</a>,
+    </div>`;
 
     const options = {
         from: process.env.ADMIN_EMAIL,
